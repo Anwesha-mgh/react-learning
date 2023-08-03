b6fe58169f9af48856822688d55484aa97fc1811
Anwesha-mgh <141143739+Anwesha-mgh@users.noreply.github.com>
2023-08-03 19:31:28 +0530

    1 file modified

--- src/components/Cake_Shop.js
+++ src/components/Cake_Shop.js
@@ -2,8 +2,11 @@ import React from 'react';
 import redux from 'redux';
 import { createStore } from '@reduxjs/toolkit';
 
+const reduxLogger = require('redux-logger');
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
@@ -79,7 +82,7 @@ const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial State', store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log('Updated State', store.getState())
